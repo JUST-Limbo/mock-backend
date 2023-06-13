0fb7f32a1414514797067eeac26bfef6a6577324
Limbo <1034935762@qq.com>
2023-06-13 19:18:00 +0800

    2023年6月13日19:18:00

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const app = express()
+const users = require("./users.json")
 
-app.get("/", (req, res) => {
+app.get("/userinfo", (req, res) => {
+	const userId = req.params.id
+	const user = users.find((u) => u.id == userId)
+	if (!user) {
+		return res.status(404).send("找不到该用户")
+	}
+	res.send(user)
 })
-
 app.listen(9600, () => {
 	console.log("服务器已启动，监听端口 9600")
+	console.log("http://localhost:9600")
 })
