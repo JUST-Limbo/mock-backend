2243133e4196c228381dc5527a0223922d6e43af
Limbo <1034935762@qq.com>
2023-06-19 13:02:52 +0800

    2023年6月19日13:02:51

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express")
+
 const app = express()
 const users = require("./users.json")
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 const axios =require('axios')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const cors = require("cors")
+app.use(cors())
 
 app.get("/userinfo", (req, res) => {
-    console.log('/userinfo');
     if(!req.cookies.userId){
         return res.status(200).send({
             // 未登录
@@ -19,6 +24,32 @@ app.get("/userinfo", (req, res) => {
         data: user,
     });
 })
+
+app.post('/queryuserlist',(req, res) => {
+    // 从请求体中获取 page 和 id 参数
+    const { page = 1, name,pageSize = 10 } = req.body;
+
+    // 根据 id 过滤 users 数组
+    const filtered = name ? users.filter(u => u.name === name) : users;
+
+    // 计算 start 和 end
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+
+    // 构造响应结果
+    const result = {
+        total: filtered.length,
+        pageSize,
+        pageCount: Math.ceil(filtered.length / pageSize),
+        page: page,
+        data: filtered.slice(start, end),
+    };
+	res.send({
+        code: '200',
+        data: result,
+    });
+})
+
 app.get("/testaxios", (req, res) => {
     console.log(axios);
     res.send('success')
